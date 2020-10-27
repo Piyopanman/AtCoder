42e4bb459c4bd3bf2ca55df90c080a6d87a2b641
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-10-27 18:01:42 +0900

    10/27

---
+++ A/main147.cpp
@@ -0,0 +1,15 @@
+//abc147
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  int a1,a2,a3;
+  cin >> a1 >> a2 >> a3;
+  if(a1 + a2 + a3 >= 22){
+    cout << "bust" << endl;
+  }else{
+    cout << "win" << endl;
+  }
+}
