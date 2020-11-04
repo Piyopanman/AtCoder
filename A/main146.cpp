0ae044a6830d931dffc2c124c1dfedcd2ae6ff37
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-11-04 13:21:11 +0900

    11/4

---
+++ A/main146.cpp
@@ -0,0 +1,25 @@
+//abc146
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  if(s == "SUN"){
+    cout << 7 << endl;
+  }else if(s == "MON"){
+    cout << 6 << endl;
+  }else if(s == "TUE"){
+    cout << 5 << endl;
+  }else if(s == "WED"){
+    cout << 4 << endl;
+  }else if(s == "THU"){
+    cout << 3 << endl;
+  }else if(s == "FRI"){
+    cout << 2 << endl;
+  }else if(s == "SAT"){
+    cout << 1 << endl;
+  }
+}
