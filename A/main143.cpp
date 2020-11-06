9d2bdf901559c16ed71c79e4e3d3ef7c9e591747
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-11-06 10:24:48 +0900

    11/6

---
+++ A/main143.cpp
@@ -0,0 +1,15 @@
+//abc143
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin >> a >> b;
+  if(a - b * 2 < 0){
+    cout << 0 << endl;
+  }else{
+    cout << a - b * 2 << endl;
+  }
+}
