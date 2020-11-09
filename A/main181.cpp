fde62f295ae748eaff0bab997b863b3e1593b61f
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-11-09 23:45:24 +0900

    11/9

---
+++ A/main181.cpp
@@ -0,0 +1,15 @@
+//abc181
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if(n % 2 == 0){
+    cout << "White" << endl;
+  }else{
+    cout << "Black" << endl;
+  }
+}
