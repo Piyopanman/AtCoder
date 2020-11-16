b2803fcfe7bd3d5fa370b6dfc444ddd10f5082f0
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-11-16 23:31:57 +0900

    11/16

---
+++ A/main138.cpp
@@ -0,0 +1,16 @@
+//abc138
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  int a;
+  string s;
+  cin >> a >> s;
+  if(a >= 3200){
+    cout << s << endl;
+  }else{
+    cout << "red" << endl;
+  }
+}
