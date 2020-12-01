ea05edf49f6f5e461d42008f9a205dfc4617565d
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-12-01 23:22:33 +0900

    12/1

---
+++ A/main136.cpp
@@ -0,0 +1,16 @@
+//abc136a
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+  int a,b,c;
+  cin >> a >> b >> c;
+  if( c - (a-b) >= 0){
+    cout <<  c - (a-b) << endl;
+  }else{
+    cout << 0 << endl;
+  }
+
+}
