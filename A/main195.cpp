2b4bc6083542a1b5ea3fc71697a48cdd2e60cbf7
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-05-24 23:49:22 +0900

    5/24

---
+++ A/main195.cpp
@@ -0,0 +1,15 @@
+//abc195 A
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+    int M, H;
+    cin >> M >> H;
+    if (H % M == 0){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
